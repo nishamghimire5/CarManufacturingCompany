de652bd0e6a386e0efe0b70adbdfaa4d6b7b104c
TheNisham <77533996+nishamghimire5@users.noreply.github.com>
2023-03-19 19:32:18 +0545

    Add files via upload

---
+++ CarDatabase.sql
@@ -0,0 +1,13 @@
+INSERT INTO Customers (customer_name, customer_email, dealer_id)
+VALUES ('John Smith', 'john.smith@example.com', 1),
+       ('Jane Doe', 'jane.doe@example.com', 2),
+       ('Bob Johnson', 'bob.johnson@example.com', 1);
+
+
+insert into customers (customer_name, customer_email, dealer_id)
+values ('Nisham Ghimire','nishamghimire5@gmail.com',1),
+       ('Aayush Adhikari','aayu@gmail.com',2),
+       ('Ballu Pandey','ballu@gmail.com',3);
+
+
+select * from customers
